1fc63aff9db5642fa004954e04cb8b665f25620f
WooKyoung Noh <wookay.noh@gmail.com>
2015-05-09 08:11:51 +0900

    DiffHead

---
+++ scripts/DiffHead.jl
@@ -0,0 +1,21 @@
+using Base.Test
+
+function diff_manual(path)
+  translated = open(readall, "../$path.txt")
+  lines = String[]
+  for line in split(translated, "\n")
+    len = length(line)
+    if len > 1
+      if "++" == line[1:2]
+      else
+        push!(lines, string(line[min(4,len+1):end], "\n"))
+      end
+    end
+  end
+  original = open(readall, "$JULIA_HOME/../../doc/$path")
+  @test original == join(lines) 
+end
+
+for path in ["manual/introduction.rst"]
+  diff_manual(path)
+end
