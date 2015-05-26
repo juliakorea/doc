9cf444d8e65c7dcf523da73865de88396dccecba
wookyoung <wookyoung@wookyoungui-iMac.local>
2015-05-27 02:17:00 +0900

    앞부분 수정

--- scripts/DocBuild.jl
+++ scripts/DocBuild.jl
@@ -1,7 +1,14 @@
 function gen_rst(path)
-  outfile = "$path"
-  out = open(outfile, "w")
-  txt = open(readall, "$path.txt")
+  target_dir = relpath(".")
+  if nothing == Base.find_in_path(normpath(target_dir, "$path.txt"))
+    target_dir = relpath("..")
+  end
+  path_txt = normpath(target_dir, "$path.txt")
+
+  out_path = normpath(target_dir, path)
+  out = open(out_path, "w")
+
+  txt = open(readall, path_txt)
   for line in split(txt, "\n")
     len = length(line)
     if len > 1
@@ -12,6 +19,7 @@ function gen_rst(path)
     end
   end
   close(out)
+  println("generated $out_path")
 end
 
 for path in ["manual/introduction.rst"]
